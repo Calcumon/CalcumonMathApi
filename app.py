3d6ae860dd2c5b1d090822c27ebea6ac9a7fbc4c
Michael McGowan <michael.mcgowan@students.makeschool.com>
2019-11-21 10:56:54 -0800

    phase1 implemented

--- app.py
+++ app.py
@@ -1,16 +1,74 @@
+import math_api
+import random
+
 from flask import Flask
 from flask import jsonify
-import math_api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
-def hello():
-    d = math_api.choose_phase()
+def index():
+    phase1 = math_api.choose_phase(1)
+    phase1_guesses = []
+    for item in phase1:
+        phase1_guesses.append(math_api.gen_guesses(0, 10, 5, item[1]))
     message = {
         'status': 200,
         'message': 'OK',
-        'scores': d
+        'phase1': [
+            {
+            'eqn1': phase1[0][0],
+            'ans1': phase1[0][1],
+            'guesses1': phase1_guesses[0],
+            },
+            {
+            'eqn2': phase1[1][0],
+            'ans2': phase1[1][1],
+            'guesses2': phase1_guesses[1]
+            },
+            {
+            'eqn3': phase1[2][0],
+            'ans3': phase1[2][1],
+            'guesses3': phase1_guesses[2]
+            },
+            {
+            'eqn4': phase1[3][0],
+            'ans4': phase1[3][1],
+            'guesses4': phase1_guesses[3]
+            },
+            {
+            'eqn5': phase1[4][0],
+            'ans5': phase1[4][1],
+            'guesses5': phase1_guesses[4]
+            },
+            {
+            'eqn6': phase1[5][0],
+            'ans6': phase1[5][1],
+            'guesses6': phase1_guesses[5]
+            },
+            {
+            'eqn7': phase1[6][0],
+            'ans7': phase1[6][1],
+            'guesses7': phase1_guesses[6]
+            },
+            {
+            'eqn8': phase1[7][0],
+            'ans8': phase1[7][1],
+            'guesses8': phase1_guesses[7]
+            },
+            ]
+        # 'phase2': [{
+        #     'eqn': p2eqn2,
+        #     'ans': p2ans2,
+        #     'guesses': p2guesses2
+        # }]
+        # 'phase3': [{
+        #     'eqn': p3eqn3,
+        #     'ans': p3ans3,
+        #     'guesses': p3guesses3
+        # }]
     }
     resp = jsonify(message)
     resp.status_code = 200
--- math_api.py
+++ math_api.py
@@ -2,26 +2,33 @@ import random
 
 def gen_eqns(int_range, num_params, step = 1):
   parameters = []
-  eqn = {}
   for _ in range(0, num_params):
     parameters.append(random.randint(1, int_range))
   indx = 0
   while indx < len(parameters):
     ans = parameters[indx] + parameters[indx + step]
-    eqn_str = str(parameters[indx]) + '+' + str(parameters[indx + 1])
-    eqn[eqn_str] = str(ans)
+    eqn_str = str(parameters[indx]) + ' + ' + str(parameters[indx + 1])
     indx += step + 1
-  return eqn
+  return eqn_str, ans
 
-def choose_phase():
-  eqns = {}
-  for _ in range(0, 6):
-    eqns.update(gen_eqns(10, 2))
-  for _ in range(0, 6):
-    eqns.update(gen_eqns(20, 2))
-  for _ in range(0, 6):
-    eqns.update(gen_eqns(20, 2))
+def choose_phase(phase):
+  eqns = []
+  if phase == 1:
+    for _ in range(9):
+      eqns.append(gen_eqns(10, 2))
+  # if phase == 2:
+  #   eqn = gen_eqn(20, 2)
+  # if phase == 3:
+  #   eqn = gen_eqn(20, 2)
   return eqns
 
+def gen_guesses(start, end, num, ans): 
+    res = []
+    while len(res) < num:
+        guess = random.randint(start, end)
+        if guess not in res and guess != ans:
+          res.append(guess)
+    return res
 
-print(choose_phase())
+
+# print(choose_phase())
